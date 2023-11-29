a42e95a918b88b77f373d51acb9d6c93cc46c056
Kirill Hrudzinski <604karev@gmail.com>
2023-11-29 09:40:31 +0100

    add signin test

--- src/__test__/routes.test.ts
+++ src/__test__/routes.test.ts
@@ -7,3 +7,15 @@ describe("GET /", () => {
     expect(res.body.message).toBe("hello world");
   });
 });
+
+describe("POST /signin", function () {
+  it("responds with json", async () => {
+    const res = await supertest(app)
+      .post("/signin")
+      .send({ username: "hellodrsdaddd", password: "holad" })
+      .set("Accept", "application/json");
+
+    expect(res.headers["content-type"]).toEqual("application/json; charset=utf-8");
+    expect(res.status).toEqual(200);
+  });
+});
